a86fd8516947c9d31c1d6f25050cd0589f9a757f
Devansh Pradhan <91042272+DevanshPradhan@users.noreply.github.com>
2023-07-22 14:03:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+// gsap.from("#headimg img",{
+//     duration:2,
+//     delay:3,
+//     opacity:0
+// })
+gsap.from("#navi h1 , #headimg h2 ,#headimg img",
+{
+    opacity:0,
+    duration:10,
+    delay:1,
+    stagger:1
+
+})
